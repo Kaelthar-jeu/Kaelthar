b8b299182f267a78baec9a180679809b53d088e9
Kaelthar-jeu <blaisingjean@gmail.com>
2026-03-09 17:37:17 +0100

    Update stripe.js

--- api/stripe.js
+++ api/stripe.js
@@ -9,7 +9,7 @@ module.exports = async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
   try {
-    const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
     const { priceId } = req.body;
 
     const session = await stripe.checkout.sessions.create({
